788952a5e454a9a1e6d3881891d21e2d7ef933fb
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-11-03 12:36:25 +0300

    Extend devise adding name and username fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 50 }
+
+  validates :email, presence: true, length: { maximum: 105 },
+                    uniqueness: { case_sensitive: false },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
