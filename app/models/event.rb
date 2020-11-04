e43ec5675eee8403ddb53c7a42ae702e53ceeee6
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-11-05 01:47:16 +0300

    Add functionality using methods and scopes in event model to separate past and upcoming events

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,10 @@ class Event < ApplicationRecord
 
   has_many :event_attendances, foreign_key: :attended_event_id
   has_many :event_attendees, through: :event_attendances, source: :event_attendee
+
+  scope :past, -> { where('start_datetime < ?', DateTime.now) }
+
+  def self.upcoming
+     where('start_datetime > ?', DateTime.now)
+  end
 end
